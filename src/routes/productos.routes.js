8a0ab6ef7b889b1f3c206f30fefaaae70ee01893
LuisDavidOR <luisdavidoporta777@gmail.com>
2025-04-03 23:17:17 -0600

    Inserción

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import {  obtenerProductos, obtenerProducto } from '../controllers/productos.controller.js';
+import {  obtenerProductos, obtenerProducto, registrarProducto } from '../controllers/productos.controller.js';
 
 const router = Router();
 
 // Ruta para obtener todos los productos
-router.get('/productos', obtenerProductos);
+router.get('/obtenerproductos', obtenerProductos);
 
 // Ruta para obtener un produto por su ID
 router.get('/producto/:id', obtenerProducto);
+
+// Ruta para Registrar un produto
+router.post('/registrarproducto', registrarProducto);
 
 export default router;
